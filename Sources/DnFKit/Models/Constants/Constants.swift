694d4575eb731af437d0a26cb1597ae3ea658680
Crying-Goose <fkauswlq789@gmail.com>
2025-07-19 23:48:45 +0900

    Create Constants.swift

---
+++ Sources/DnFKit/Models/Constants/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  DnFKit
+//
+//  Created by 라주영 on 7/15/25.
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum Rarity {
+        static let common = "커먼"
+        static let uncommon = "언커먼"
+        static let rare = "레어"
+        static let unique = "유니크"
+        static let legendary = "레전더리"
+        static let epic = "에픽"
+        static let beginning = "태초"
+    }
+}
