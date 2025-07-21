f73ac43f0200e904639a335c8bab76838a002119
Crying-Goose <fkauswlq789@gmail.com>
2025-07-21 12:46:23 +0900

    Update DnFRepository.swift

--- Sources/DnFKit/Repositories/DnFRepository.swift
+++ Sources/DnFKit/Repositories/DnFRepository.swift
@@ -10,7 +10,14 @@ import Foundation
 public protocol DnFRepositoryProtocol {
     func fetchDnFCharacters(name: String) async throws -> [JustCharacter]
     func fetchDnFCharacter(server: String, name: String) async throws -> JustCharacter
-    func fetchDnFCharacterInfo(server: String, id: String) async throws -> CharacterInfo
+    func fetchDnFCharacterInfo(server: String, id: String) async throws -> Character
+    func fetchDnFCharacterStatus(server: String, id: String) async throws -> Status
+    func fetchDnFCharacterEquipment(server: String, id: String) async throws -> [Equipment]
+    func fetchDnFCharacterSkillStyle(server: String, id: String) async throws -> SkillStyle
+    func fetchDnFCharacterAvatar(server: String, id: String) async throws -> [Avatar]
+    func fetchDnFCharacterFlag(server: String, id: String) async throws -> Flag
+    func fetchDnFCharacterBuffs(server: String, id: String) async throws -> [Buff]
+    func fetchDnFChracterCreature(server: String, id: String) async throws -> Creature
     func fetchDnFTimeline(server: String, id: String) async throws -> Timeline
 }
 
@@ -34,39 +41,67 @@ public final class DnFRepository: DnFRepositoryProtocol {
         return JustCharacter(dto: response.rows[0])
     }
     
-    public func fetchDnFCharacterInfo(server: String, id: String) async throws -> CharacterInfo {
-        let baseInfoDTO = try await apiService.getCharacterInfo(server: server, id: id)
-        let statusDTO = try await apiService.getStatus(server: server, id: id)
-        let equipmentDTO = try await apiService.getEquipment(server: server, id: id)
-        let avatarDTO = try await apiService.getAvatar(server: server, id: id)
-        let creatureDTO = try await apiService.getCreature(server: server, id: id)
-        let flagDTO = try await apiService.getFlag(server: server, id: id)
-        let skillDTO = try await apiService.getSkills(server: server, id: id)
-        let buffEquipmentDTO = try await apiService.getBuffEquipment(server: server, id: id)
-        let buffAvalarDTO = try await apiService.getBuffAvatar(server: server, id: id)
-        let buffCreatureDTO = try await apiService.getBuffCreature(server: server, id: id)
-        var buffs: [Buff] = []
-        buffEquipmentDTO.skill.buff.equipment?.forEach {
-            buffs.append(.init(equipmentDto: $0))
-        }
-        buffAvalarDTO.skill.buff.avatar?.forEach {
-            buffs.append(.init(avatarDto: $0))
-        }
-        buffCreatureDTO.skill.buff.creature?.forEach {
-            buffs.append(.init(creatureDto: $0))
-        }
-        return .init(
-            baseInfo: .init(dto: baseInfoDTO),
-            status: .init(dto: statusDTO),
-            equipment: equipmentDTO.equipment.map { .init(dto: $0) },
-            avatar: avatarDTO.avatar.map { .init(dto: $0) },
-            creature: .init(dto: creatureDTO.creature),
-            flag: .init(dto: flagDTO.flag),
-            buffs: buffs,
-            skill: .init(dto: skillDTO.skill)
-        )
+    // 캐릭터 정보 검색
+    public func fetchDnFCharacterInfo(server: String, id: String) async throws -> Character {
+        let response = try await apiService.getCharacterInfo(server: server, id: id)
+        return .init(dto: response)
     }
     
+    // 캐릭터 스탯 검색
+    public func fetchDnFCharacterStatus(server: String, id: String) async throws -> Status {
+        let response = try await apiService.getStatus(server: server, id: id)
+        return .init(dto: response)
+    }
+    
+    // 캐릭터 장비 검색
+    public func fetchDnFCharacterEquipment(server: String, id: String) async throws -> [Equipment] {
+        let response = try await apiService.getEquipment(server: server, id: id)
+        return response.equipment.compactMap { .init(dto: $0) }
+    }
+    
+    // 캐릭터 스킬 검색
+    public func fetchDnFCharacterSkillStyle(server: String, id: String) async throws -> SkillStyle {
+        let response = try await apiService.getSkills(server: server, id: id)
+        return .init(dto: response.skill)
+    }
+    
+    // 캐릭터 아바타 검색
+    public func fetchDnFCharacterAvatar(server: String, id: String) async throws -> [Avatar] {
+        let response = try await apiService.getAvatar(server: server, id: id)
+        return response.avatar.compactMap { .init(dto: $0) }
+    }
+    
+    // 캐릭터 휘장 검색
+    public func fetchDnFCharacterFlag(server: String, id: String) async throws -> Flag {
+        let response = try await apiService.getFlag(server: server, id: id)
+        return .init(dto: response.flag)
+    }
+    
+    // 캐릭터 버프 강화 검색
+    public func fetchDnFCharacterBuffs(server: String, id: String) async throws -> [Buff] {
+        let equipmentResponse = try await apiService.getBuffEquipment(server: server, id: id)
+        let avatarResponse = try await apiService.getBuffAvatar(server: server, id: id)
+        let creatureResponse = try await apiService.getBuffCreature(server: server, id: id)
+        var buffs: [Buff] = []
+        equipmentResponse.skill.buff.equipment?.forEach {
+            buffs.append(.init(equipmentDto: $0))
+        }
+        avatarResponse.skill.buff.avatar?.forEach {
+            buffs.append(.init(avatarDto: $0))
+        }
+        creatureResponse.skill.buff.creature?.forEach {
+            buffs.append(.init(creatureDto: $0))
+        }
+        return buffs
+    }
+    
+    // 캐릭터 크리쳐 검색
+    public func fetchDnFChracterCreature(server: String, id: String) async throws -> Creature {
+        let response = try await apiService.getCreature(server: server, id: id)
+        return .init(dto: response.creature)
+    }
+    
+    // 캐릭터 타임라인 검색
     public func fetchDnFTimeline(server: String, id: String) async throws -> Timeline {
         let date = Date()
         // 날짜 계산해서 캐릭터 생성일 전까지 계속 루프
